ae7469e7a491ab65fc954b6a94a92e1bbcf58f4e
Andrew Zaki <aezaki@uwaterloo.ca>
2026-05-15 08:58:36 -0400

    fixed logging in packaging

--- app/main.py
+++ app/main.py
@@ -997,4 +997,10 @@ def serve_react_app(full_path: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(
+        app,
+        host="127.0.0.1",
+        port=8000,
+        log_config=None,
+        access_log=False,
+    )
